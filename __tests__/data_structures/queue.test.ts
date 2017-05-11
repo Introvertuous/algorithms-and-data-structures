f1b529f3d3373a0a63f21129a0e277c07d7fb8bb
Chris Ryan <theramin108@gmail.com>
2017-05-10 22:16:58 -0400

    update packages

--- __tests__/data_structures/queue.test.ts
+++ __tests__/data_structures/queue.test.ts
@@ -8,7 +8,7 @@ test('queue', () => {
     queue.enqueue('one');
     queue.enqueue('two');
     queue.enqueue('three');
-    
+
     expect(queue.dequeue()).toBe('one');
     expect(queue.dequeue()).toBe('two');
     expect(queue.dequeue()).toBe('three');
