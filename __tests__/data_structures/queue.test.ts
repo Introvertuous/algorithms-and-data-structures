65d4119cef5edae129fecf706f6e0b1805eb2b83
Chris Ryan <theramin108@gmail.com>
2017-05-22 16:52:16 -0400

    formatting

--- __tests__/data_structures/queue.test.ts
+++ __tests__/data_structures/queue.test.ts
@@ -3,13 +3,12 @@
 import { Queue } from '../../data_structures';
 
 test('queue', () => {
-    const queue = new Queue();
-
-    queue.enqueue('one');
-    queue.enqueue('two');
-    queue.enqueue('three');
-
-    expect(queue.dequeue()).toBe('one');
-    expect(queue.dequeue()).toBe('two');
-    expect(queue.dequeue()).toBe('three');
+  const queue = new Queue();
+  
+  queue.enqueue('one');
+  queue.enqueue('two');
+  queue.enqueue('three');
+  expect(queue.dequeue()).toBe('one');
+  expect(queue.dequeue()).toBe('two');
+  expect(queue.dequeue()).toBe('three');
 });
