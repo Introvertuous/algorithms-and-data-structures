c556c51588f3c8f47ef4be6e8835202ec459587a
Chris <theramin108@gmail.com>
2017-09-06 12:43:32 -0400

    opt for global prettier

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import * as DataStructures from "./src/data_structures";
-import * as Algorithms from "./src/data_structures";
+import * as DataStructures from './src/data_structures';
+import * as Algorithms from './src/data_structures';
 
 export default { DataStructures, Algorithms };
